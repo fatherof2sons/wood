ccc92f9c60ad3acb4a2096a6aeecb87bc8d732f9
fatherof2sons <1613azeen1320@gmail.com>
2021-08-09 21:15:41 +0500

    working on about page -- not final

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fancy_drawer/fancy_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(App());
@@ -15,6 +16,8 @@ class App extends StatelessWidget {
     );
   }
 }
+
+enum OnPage { home, about, contact, social }
 
 class HomePage extends StatefulWidget {
   @override
@@ -41,6 +44,8 @@ class _HomePageState extends State<HomePage>
     super.dispose();
   }
 
+  OnPage _onPage = OnPage.home;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -50,12 +55,22 @@ class _HomePageState extends State<HomePage>
         drawerItems: _buildItems(context),
         child: SafeArea(
           child: Scaffold(
-            backgroundColor: Colors.white,
+            backgroundColor: Colors.grey.shade50,
             appBar: AppBar(
               elevation: 0.0,
-              backgroundColor: Colors.white,
+              backgroundColor: Colors.grey.shade50,
               centerTitle: true,
-              title: IconButton(
+              title: Icon(
+                _onPage == OnPage.home
+                    ? FontAwesomeIcons.home
+                    : _onPage == OnPage.about
+                        ? FontAwesomeIcons.info
+                        : _onPage == OnPage.contact
+                            ? FontAwesomeIcons.mailBulk
+                            : FontAwesomeIcons.connectdevelop,
+                color: Colors.black87,
+              ),
+              leading: IconButton(
                 icon: Icon(
                   Icons.menu,
                   color: Colors.black,
@@ -66,26 +81,42 @@ class _HomePageState extends State<HomePage>
                 },
               ),
             ),
-            body: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  alignment: Alignment.bottomCenter,
-                  image: AssetImage("images/wood_art.jpg"),
-                ),
-              ),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 60.0),
-                  child: Text(
-                    "The Wood",
-                    style: TextStyle(
-                      fontFamily: "Bowlby One SC",
-                      fontSize: 62.0,
-                      color: Colors.black87,
-                    ),
-                  ),
-                ),
-              ),
+            body: _buildContent(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    if (_onPage == OnPage.home) {
+      return _home();
+    } else if (_onPage == OnPage.about) {
+      return _about();
+    } else if (_onPage == OnPage.contact) {
+      return _contact();
+    } else {
+      return _social();
+    }
+  }
+
+  Widget _home() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          alignment: Alignment.bottomCenter,
+          image: AssetImage("images/wood_art.jpg"),
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 90.0),
+          child: Text(
+            "The Wood",
+            style: TextStyle(
+              fontFamily: "Bowlby One SC",
+              fontSize: 42.0,
+              color: Colors.black87,
             ),
           ),
         ),
@@ -93,12 +124,169 @@ class _HomePageState extends State<HomePage>
     );
   }
 
+  Widget _about() {
+    final primaryStyle = TextStyle(
+      fontSize: 40.0,
+      letterSpacing: 1.2,
+      fontFamily: "Bowlby One SC",
+      color: Colors.black87,
+    );
+    final secondaryStyle = TextStyle(
+      fontSize: 24.0,
+      letterSpacing: 1,
+    );
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40.0),
+            height: 400.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              // border: Border.all(),
+              color: Colors.white,
+              image: DecorationImage(
+                // alignment: Alignment.topCenter,
+                fit: BoxFit.fitWidth,
+                image: AssetImage("images/about_art.jpeg"),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.only(
+              top: 40.0,
+              right: 12.0,
+              bottom: 30.0,
+              left: 12.0,
+            ),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: "The Wood ",
+                    style: primaryStyle,
+                  ),
+                  TextSpan(
+                    text:
+                        "is the best Maldivian hard rock band from capital city Male' formed in 2006. It consists of brothers vocalist Ammadey bassist Mo, guitarists Hamza and Shifau, Zahil on keys and Azeen on drums.",
+                    style: secondaryStyle,
+                  )
+                ],
+              ),
+              textAlign: TextAlign.justify,
+              style: secondaryStyle,
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding:
+                const EdgeInsets.symmetric(vertical: 40.0, horizontal: 12.0),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: "The Wood brings ",
+                    style: secondaryStyle,
+                  ),
+                  TextSpan(
+                    text: "music ",
+                    style: primaryStyle,
+                  ),
+                  TextSpan(
+                    text:
+                        "that electrifies the audience in awe with their mesmerizing performance. The energetic attitude and presence make sure that the guests are well entertained.",
+                    style: secondaryStyle,
+                  )
+                ],
+              ),
+              textAlign: TextAlign.justify,
+              style: secondaryStyle,
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding:
+                const EdgeInsets.symmetric(vertical: 30.0, horizontal: 12.0),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text:
+                        "Their relationship with the clients they have performed for has been rated as a great ",
+                    style: secondaryStyle,
+                  ),
+                  TextSpan(
+                    text: "bond ",
+                    style: primaryStyle,
+                  ),
+                  TextSpan(
+                    text:
+                        "and understanding because of their enigmatic performance.",
+                    style: secondaryStyle,
+                  )
+                ],
+              ),
+              textAlign: TextAlign.justify,
+              style: secondaryStyle,
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding:
+                const EdgeInsets.symmetric(vertical: 30.0, horizontal: 12.0),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: "The various variety of ",
+                    style: secondaryStyle,
+                  ),
+                  TextSpan(
+                    text: "musical genres ",
+                    style: primaryStyle,
+                  ),
+                  TextSpan(
+                    text:
+                        "they play from Classic rock, Latin, RnB and the latest billboard music is why the wood has been around for more than a decade.",
+                    style: secondaryStyle,
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.justify,
+              style: secondaryStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _contact() {
+    return Container(
+        child: Center(
+      child: Text("Contact"),
+    ));
+  }
+
+  Widget _social() {
+    return Container(
+        child: Center(
+      child: Text("Contact"),
+    ));
+  }
+
+  void _pageState(OnPage onPage) {
+    setState(() => _onPage = onPage);
+    _controller.close();
+  }
+
   List<Widget> _buildItems(BuildContext context) {
     return [
-      Links("Home", () {}),
-      Links("About", () {}),
-      Links("Contact", () {}),
-      Links("Social", () {}),
+      Links("Home", () => _pageState(OnPage.home)),
+      Links("About", () => _pageState(OnPage.about)),
+      Links("Contact", () => _pageState(OnPage.contact)),
+      Links("Social", () => _pageState(OnPage.social)),
     ];
   }
 }
